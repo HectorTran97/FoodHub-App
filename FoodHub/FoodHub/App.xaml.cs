cfc50d11cbdce4b15a1e015fec8aee7ef2a88276
pceu <developer.aceu@gmail.com>
2019-09-03 10:22:51 +1000

    group pages (home, favourite, map, account) into tabbed page

--- FoodHub/FoodHub/App.xaml.cs
+++ FoodHub/FoodHub/App.xaml.cs
@@ -12,7 +12,18 @@ namespace FoodHub
         {
             InitializeComponent();
             // Set the login page as main page
-            MainPage = new NavigationPage(new LoginPage());
+            // (Ceu) I'll now comment this as I need to work on the other pages
+            // we will put them in if statement later to decide which (login page or tabbed page) page to start with
+            // feel free to comment tabbedpage section and run this login page first
+            //MainPage = new NavigationPage(new LoginPage());
+
+            var tabbedPage = new TabbedPage();
+            tabbedPage.Children.Add(new HomePage());
+            tabbedPage.Children.Add(new FavouritePage());
+            tabbedPage.Children.Add(new UserMapPage());
+            tabbedPage.Children.Add(new AccountPage());
+            MainPage = tabbedPage;
+
         }
 
         protected override void OnStart()
@@ -28,6 +39,9 @@ namespace FoodHub
         protected override void OnResume()
         {
             // Handle when your app resumes
+            // we will try to track the state of login or logout
+            // if loggedin state = present user to tabbed page (main page)
+            // if logout state = present user to login page
         }
     }
 }
