05a3319727d11875352da101252ce2ce447f632f
pceu <developer.aceu@gmail.com>
2019-09-11 15:08:18 +1000

    merge

--- FoodHub/FoodHub/View/RestaurantDetailsPage.xaml.cs
+++ FoodHub/FoodHub/View/RestaurantDetailsPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,17 @@ namespace FoodHub.View
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class RestaurantDetailsPage : ContentPage
     {
+        private ObservableCollection<Menu> menuList;
         public RestaurantDetailsPage(int id, string name)
         {
             InitializeComponent();
             SampleLabelID.Text = "Restaurant ID: " + id.ToString();
             SampleLabelName.Text = "Name: " + name;
+        }
+
+        public class Menu
+        {
+
         }
     }
 }
